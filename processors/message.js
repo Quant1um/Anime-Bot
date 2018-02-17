867cbb661e6566d7dd94e29e272b771f584dadc5
Quant1um <spitfirexv22@gmail.com>
2018-02-17 12:42:11 +0300

    ....

--- processors/message.js
+++ processors/message.js
@@ -1,4 +1,7 @@
 module.exports = function(data){
-	data.vk.api.messages.send(data.object.user_id, "test message");
+	data.vk.api.call("messages.send", {
+		user_id: data.object.user_id,
+		message: "incoming message"
+	});
 	data.end(200, "ok");
 };
