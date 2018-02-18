8300fe1108aa37c7a15097cddc431062c4b0116c
Quant1um <spitfirexv22@gmail.com>
2018-02-18 19:50:10 +0300

    Fix

--- processors/allow.js
+++ processors/allow.js
@@ -1,4 +1,4 @@
 module.exports = function(data){
-	data.vk.send(data.object.user_id, "message_allow: " + JSON.stringify(data.object));
+	data.api.send(data.object.user_id, "message_allow: " + JSON.stringify(data.object));
 	data.end(200, "ok");
 };
