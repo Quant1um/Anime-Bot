def8f21d934bfe84bee18fcde8ac8254747fa3f5
Quant1um <spitfirexv22@gmail.com>
2018-10-27 11:27:08 +0300

    Changed booru to 'yandere' (request to danbooru with overridden rating sometimes crashes app for whatever reason)

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-﻿let booru = require("booru");
-booru.search("danbooru", ["rating:e"], { limit: 1, random: true }).then((e) => console.log(e, "e"));
-booru.search("danbooru", ["rating:s"], { limit: 1, random: true }).then((e) => console.log(e, "s"));
-booru.search("danbooru", ["cute", "rating:s"], { limit: 1, random: true }).then((e) => console.log(e, "cutes"));
-booru.search("danbooru", ["cute", "rating:e"], { limit: 1, random: true }).then((e) => console.log(e, "cutee"));
-//require("./src/index.js");
+﻿require("./src/index.js");
